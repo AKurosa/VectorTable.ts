48b96ef9e97d206a8c6b5cfb734e62095ace7080
AKurosa <98587872+AKurosa@users.noreply.github.com>
2022-03-08 20:52:42 +0900

    Add Checking Target Id

--- build/VectorTable.js
+++ build/VectorTable.js
@@ -1,5 +1,25 @@
 "use strict";
-function greet(name) {
-    return `Hello, ${name}!`;
+class VectorTable {
+    checkingTargetId(id) {
+        const element = document.getElementById(id);
+        if (element === null) {
+            throw new Error('No element id=' + id);
+        }
+    }
 }
-console.log(greet("Vector Table"));
+/**
+ * Drow Table using SVG.
+ * @param  {string} id Target id
+ * @param  {object} setting Drow Setting
+ * @param  {Array<Array<object>>} head Table head
+ * @param  {Array<Array<string>>} body Table body
+ */
+function addVectorTable(id, setting, head, body) {
+    try {
+        let vectorTable = new VectorTable();
+        vectorTable.checkingTargetId(id);
+    }
+    catch (error) {
+        throw new Error(error + ' [vectorTable]');
+    }
+}
--- src/VectorTable.ts
+++ src/VectorTable.ts
@@ -1,4 +1,23 @@
-function greet(name: string): string {
-    return `Hello, ${name}!`;
+class VectorTable{
+    checkingTargetId(id: string): void{
+        const element: HTMLInputElement = <HTMLInputElement>document.getElementById(id);
+        if(element===null){
+            throw new Error('No element id=' + id);
+        }
+    }
 }
-console.log(greet("Vector Table"));
+/**
+ * Drow Table using SVG.
+ * @param  {string} id Target id
+ * @param  {object} setting Drow Setting
+ * @param  {Array<Array<object>>} head Table head
+ * @param  {Array<Array<string>>} body Table body
+ */
+function addVectorTable(id: string, setting: object, head: Array<Array<object>>, body: Array<Array<string>>): void{
+    try{
+        let vectorTable: VectorTable = new VectorTable();
+        vectorTable.checkingTargetId(id);
+    }catch(error){
+        throw new Error(error + ' [vectorTable]');
+    }
+}
